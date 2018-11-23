b9f6835eeeaa2d0f84359fa9c17591ca95cdcea2
Benamira Adrien <adrien.benamira@supelec.fr>
2018-11-23 14:41:23 +0100

    correct bugs extract features

--- tools/extract_features.py
+++ tools/extract_features.py
@@ -3,62 +3,80 @@ import h5py
 import numpy as np
 
 
-df = pd.read_csv('../dataset/train_y.csv')
-train_file = h5py.File('../dataset/train.h5', 'r')
 
-index_labels={i : df.index[df.sleep_stage==i].tolist() for i in range(5)}
-taille_min_sac = len(index_labels[1])
-sample = [np.random.choice(index_labels[i], taille_min_sac) for i in range(5)]
-all_samples = np.array([i for j in sample for i in j])
-np.random.shuffle(all_samples)
-train_samples = all_samples[:int(0.8*len(all_samples))]
-val_samples = all_samples[int(0.8*len(all_samples)):]
+def poser_base():
+    df = pd.read_csv('../dataset/train_y.csv')
+    index_labels = {i: df.id[df.sleep_stage == i].tolist() for i in range(5)}
+    taille_min_sac = len(index_labels[1])
+    sample = [np.random.choice(index_labels[i], taille_min_sac) for i in range(5)]
+    all_samples = np.array([i for j in sample for i in j])
+    np.random.shuffle(all_samples)
+    train_samples = all_samples[:int(0.8 * len(all_samples))]
+    val_samples = all_samples[int(0.8 * len(all_samples)):]
+    return(train_samples, val_samples)
 
-
-def extract_features(train_samples):
-    X_300 = np.zeros((4, len(train_samples), 300))
-    X_1500 = np.zeros((len(list(train_file.keys())) - 4, len(train_samples), 1500))
-    Y = np.zeros(len(train_samples))
+def extract_data(num):
+    train_file = h5py.File('../dataset/train.h5', 'r')
     items = list(train_file.keys())
-    compteur_300 = 0
-    compteur_1500 = 0
+    TF = {}
     for index_item, item in enumerate(items):
-        train_fi = train_file[item][:]
-        print(train_fi.shape)
-        if train_fi.shape[1] == 300:
-            X_300[compteur_300] = np.take(train_fi, train_samples, axis=0)
-            compteur_300 += 1
-        else:
-            X_1500[compteur_1500] = np.take(train_fi, train_samples, axis=0)
-            compteur_1500 += 1
-    for index_sample, val_sample in enumerate(train_samples):
-        Y[index_sample] = df.sleep_stage[val_sample]
-    return(X_300, X_1500, Y)
+        print(train_file[item][:].shape[1])
+        if train_file[item][:].shape[1] == num:
+            TF[item] = train_file[item][:]
+    train_file.close()
+    return TF
 
-X_300, X_1500, Y = extract_features(train_samples)
-X_300_val, X_1500_val, Y_val = extract_features(val_samples)
+def finishing(train_samples, val_samples, TF, dimf, taille):
+    X_train = {i: 0 for i in train_samples}
+    for num in X_train.keys():
+        all_data = []
+        for index_item, item in enumerate(TF.keys()):
+            all_data.append(list(TF[item][num]))
+        X_train[num] = all_data
+    X_val = {i: 0 for i in val_samples}
+    for num in X_val.keys():
+        all_data = []
+        for index_item, item in enumerate(TF.keys()):
+            all_data.append(list(TF[item][num]))
+        X_val[num] = all_data
+    Y_df = pd.read_csv('../dataset/train_y.csv')
 
-X_300.shape
-X_300s = X_300.reshape(5412,4,300)
-print(X_300.shape)
+    del TF
 
-print(X_1500.shape)
-X_1500s = X_1500.reshape(5412,7,1500)
-print(X_1500.shape)
+    df2 = pd.DataFrame(X_train)
+    X_train_f = np.zeros((len(df2.keys()), dimf, taille))
+    Y_train_f = np.zeros((len(df2.keys())))
+    for j in range(len(df2.keys())):
+        for k in range(7):
+            st = df2[df2.keys()[j]][k]
+            X_train_f[j][k][:] = st
+        Y_train_f[j] = Y_df.sleep_stage[int(df2.keys()[j])]
+    df3 = pd.DataFrame(X_val)
+    X_val_f = np.zeros((len(df3.keys()), dimf, taille))
+    Y_val_f = np.zeros((len(df3.keys())))
+    for j in range(len(df3.keys())):
+        for k in range(7):
+            st = df3[df3.keys()[j]][k]
+            X_val_f[j][k][:] = st
+        Y_val_f[j] = Y_df.sleep_stage[int(df3.keys()[j])]
 
-print(X_300_val.shape)
-X_300_vals = X_300_val.reshape(1353,4,300)
-print(X_300_val.shape)
+    return(df2, df3, X_train_f, X_val_f, Y_train_f, Y_val_f)
 
-X_1500_val.shape
-X_1500_vals = X_1500_val.reshape(1353,7,1500)
-print(X_1500_val.shape)
+train_samples, val_samples = poser_base()
+TF1500 = extract_data(1500)
+(X_train_df_1500, X_val_df_1500, X_train_f1500, X_val_f1500, Y_train_f, Y_val_f) = finishing(train_samples, val_samples, TF1500, 7, 1500)
+TF300 = extract_data(300)
+(X_train_df_300, X_val_df_300, X_train_f300, X_val_f300, _, _) = finishing(train_samples, val_samples, TF300, 4, 300)
 
-np.save("../dataset/X_features_train_equilibrate_300.npy", X_300s)
-np.save("../dataset/X_features_train_equilibrate_1500.npy", X_1500s)
-np.save("../dataset/Y_labels_train_equilibrate.npy", Y)
-np.save("../dataset/X_features_val_equilibrate_300.npy", X_300_vals)
-np.save("../dataset/X_features_val_equilibrate_1500.npy", X_1500_vals)
-np.save("../dataset/Y_labels_val_equilibrate.npy", Y_val)
 
-train_file.close()
+
+X_train_df_1500.to_csv("../dataset/X_features_train_equilibrate_1500.csv")
+X_val_df_1500.to_csv("../dataset/X_features_val_equilibrate_1500.csv")
+X_train_df_300.to_csv("../dataset/X_features_train_equilibrate_300.csv")
+X_val_df_300.to_csv("../dataset/X_features_val_equilibrate_300.csv")
+np.save("../dataset/X_features_train_equilibrate_300.npy", X_train_f300)
+np.save("../dataset/X_features_train_equilibrate_1500.npy", X_train_f1500)
+np.save("../dataset/Y_labels_train_equilibrate.npy", Y_train_f)
+np.save("../dataset/X_features_val_equilibrate_300.npy", X_val_f300)
+np.save("../dataset/X_features_val_equilibrate_1500.npy", X_val_f1500)
+np.save("../dataset/Y_labels_val_equilibrate.npy", Y_val_f)
